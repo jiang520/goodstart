678b0bf89d240ab77292a374d7ad6a20aa8ef25b
jiang <ericwill@163.com>
2013-10-10 00:32:18 +0800

    [chg]移除pyc文件,添加新增加的文件

---
+++ python_study/src/ims/DlgLogin.py
@@ -0,0 +1,73 @@
+#encoding=gb2312
+'''
+Created on 2013-9-27
+
+@author: Administrator
+'''
+
+from PyQt4.QtGui import *
+import sys
+from ims.model.dbSysUser import *
+from ims.ui.uiDlgLogin import *
+from PyQt4.QtCore import *
+from ims.model.dbClient import *
+class DlgLogin(QDialog):
+    '''
+    classdocs
+    '''
+    def __init__(self,parent, bUsedForChooseClient=False):
+        super(QDialog,self).__init__(parent)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+        self.setWindowTitle(u'欢迎使用')
+        self.__initUserNameList()
+        self.ui.commandLinkButton_register.clicked.connect(self.slotRegister)
+        self.ui.pushButton_load.clicked.connect(self.slotLogin)
+        #self.setStyleSheet('backgroud-image:images/bkgnd.jpg')
+
+
+
+    #初始化用户名列表
+    def __initUserNameList(self):
+        font = QFont()
+        font.setFamily(u'微软雅黑')
+        font.setPixelSize(13)
+        font.setBold(True)
+        self.ui.comboBox_username.setFont(font)
+        self.ui.lineEdit_pass.setFont(font)
+
+        usernames = [u'admin',u'jiang',u'hou']
+        for name in usernames:
+            self.ui.comboBox_username.insertItem(0, name)
+        self.ui.lineEdit_pass.setText(u'5950ut')
+    def slotRegister(self):
+        QMessageBox.critical(self, u'error', u'暂时不提供注册')
+        pass
+
+    def slotLogin(self):
+        strUserName = u'%s'%self.ui.comboBox_username.currentText()
+        strPass     = u'%s'%self.ui.lineEdit_pass.text()
+        if strUserName == '':
+            self.ui.comboBox_username.setFocus()
+            return
+        if strPass == '':
+            self.ui.lineEdit_pass.setFocus()
+            return
+        userinfo = SysUser()
+        userinfo.username = strUserName
+        userinfo.password = strPass
+        res = dbSysUser().isValidUser(userinfo)
+        print 'res = ',res
+        if not res:
+            QMessageBox.critical(self, 'info', u'帐户或密码输误错误')
+            self.ui.lineEdit_pass.setFocus()
+        else:
+            global g_current_user
+            g_current_user = userinfo
+            self.accept()
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    window = DlgLogin(None)
+    window.show()
+    app.exec_()
---
+++ python_study/src/ims/ui/uiDlgLogin.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'DlgLogin.ui'
+#
+# Created: Thu Oct 10 00:00:54 2013
+#      by: PyQt4 UI code generator 4.10.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(400, 224)
+        self.groupBox = QtGui.QGroupBox(Dialog)
+        self.groupBox.setGeometry(QtCore.QRect(50, 20, 291, 141))
+        self.groupBox.setTitle(_fromUtf8(""))
+        self.groupBox.setObjectName(_fromUtf8("groupBox"))
+        self.comboBox_username = QtGui.QComboBox(self.groupBox)
+        self.comboBox_username.setGeometry(QtCore.QRect(40, 30, 200, 31))
+        self.comboBox_username.setEditable(True)
+        self.comboBox_username.setObjectName(_fromUtf8("comboBox_username"))
+        self.lineEdit_pass = QtGui.QLineEdit(self.groupBox)
+        self.lineEdit_pass.setGeometry(QtCore.QRect(40, 70, 200, 30))
+        self.lineEdit_pass.setInputMethodHints(QtCore.Qt.ImhHiddenText)
+        self.lineEdit_pass.setObjectName(_fromUtf8("lineEdit_pass"))
+        self.checkBox_recordpass = QtGui.QCheckBox(self.groupBox)
+        self.checkBox_recordpass.setGeometry(QtCore.QRect(60, 110, 71, 16))
+        self.checkBox_recordpass.setObjectName(_fromUtf8("checkBox_recordpass"))
+        self.checkBox_autoload = QtGui.QCheckBox(self.groupBox)
+        self.checkBox_autoload.setGeometry(QtCore.QRect(140, 110, 71, 16))
+        self.checkBox_autoload.setObjectName(_fromUtf8("checkBox_autoload"))
+        self.pushButton_load = QtGui.QPushButton(Dialog)
+        self.pushButton_load.setGeometry(QtCore.QRect(100, 170, 151, 41))
+        self.pushButton_load.setObjectName(_fromUtf8("pushButton_load"))
+        self.commandLinkButton_register = QtGui.QCommandLinkButton(Dialog)
+        self.commandLinkButton_register.setGeometry(QtCore.QRect(260, 170, 111, 41))
+        self.commandLinkButton_register.setObjectName(_fromUtf8("commandLinkButton_register"))
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.checkBox_recordpass.setText(_translate("Dialog", "记住密码", None))
+        self.checkBox_autoload.setText(_translate("Dialog", "自动登录", None))
+        self.pushButton_load.setText(_translate("Dialog", "登        录", None))
+        self.commandLinkButton_register.setText(_translate("Dialog", "注册", None))
+
