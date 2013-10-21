efc96bf7a0a9308b35d4ad28373cf86e8d57c07e
jiang <ericwill@163.com>
2013-10-21 21:54:11 +0800

    [chg]增加了用户登录数据库创建 [chg]更新了数据表格的创建代码

--- python_study/src/ims/model/dbActicleIMS.py
+++ python_study/src/ims/model/dbActicleIMS.py
@@ -4,6 +4,8 @@ Created on 2013-9-27
 
 @author: Administrator
 '''
+import sys
+import os
 import sqlite3
 class dbActicleIMS(object):
     '''
@@ -24,32 +26,26 @@ class dbActicleIMS(object):
 
     '''创建数据库'''
     def createDataBase(self):
-        import sys
-        import os
+
         self.__dbFilePath = u"%s\\ims_acticle.db3"%os.getcwdu()
         print self.__dbFilePath
         print '======open data base at path:%s'%self.__dbFilePath
         self.con = sqlite3.connect(self.__dbFilePath)
-        sql_cr_tbtyp = ''' CREATE TABLE if not exists tbType
-                        (
-                        id integer primary key autoincrement,
-                        name varchar(100),
-                        parentid uint
+        sql_cr_tbtyp = ''' CREATE TABLE if not exists tbType  ( id integer primary key autoincrement,
+                                                name varchar(100),
+                                                parentid uint
                         );'''
-        sql_cr_tbInOut = '''CREATE TABLE if not exists tbInOutRecord 
-                        (
-                        id integer primary key autoincrement,
-                        articleid integer not null,
-                        time datetime not null,                    
-                        count float not null,
-                        price float,
-                        recordid uint not null,
-                        clientid uint,
-                        detail varchar(100)
-                        );
-                        '''
-        sql_cr_tbclient = '''CREATE TABLE if not exists tbClient
-                            (
+        sql_cr_tbInOut = '''CREATE TABLE if not exists tbInOutRecord ( id integer primary key autoincrement,
+                                                    articleid integer not null,
+                                                    time datetime not null,
+                                                    count float not null,
+                                                    price float,
+                                                    recordid uint not null,
+                                                    clientid uint,
+                                                    detail varchar(100)
+                                                    );
+                                                    '''
+        sql_cr_tbclient = '''CREATE TABLE if not exists tbClient (
                             id integer primary key autoincrement,
                             name varchar(100),
                             address varchar(200),
@@ -59,19 +55,19 @@ class dbActicleIMS(object):
                             clienttype varchar(50),
                             detail varchar(100)
                             );'''
-        sql_cr_tbArticle = '''CREATE TABLE if not exists tbArticle(
-                            id integer primary key autoincrement,
-                            typeid    uint,
-                            model     varchar(50),
-                            packaging varchar(100),
-                            pingpai   varchar(100),
-                            function  varchar(100),
-                            detail    varchar(200)
-                            );'''
-        sql_cr_tbSysUser = '''CREATE TABLE tbSysUser(
-                                id integer  primary key autoincrement,
-                                username varchar(50) not null unique,
-                                password varchar(100) not null);'''
+        sql_cr_tbArticle = '''CREATE TABLE if not exists tbArticle( id integer primary key autoincrement,
+                                                    typeid    uint,
+                                                    model     varchar(50),
+                                                    packaging varchar(100),
+                                                    pingpai   varchar(100),
+                                                    function  varchar(100),
+                                                    detail    varchar(200),
+                                                    unit varchar(20)
+                                                     );'''
+        sql_cr_tbSysUser = '''CREATE TABLE if not exists tbSysUser( id integer  primary key autoincrement,
+                                                    username varchar(50) not null unique,
+                                                    password varchar(100) not null,
+                                                    usertype varchar(50) not null);'''
         import  base64
         passd = u'5950ut'
         pass_encoded = base64.encodestring(passd)
@@ -82,6 +78,7 @@ class dbActicleIMS(object):
         self.con.execute(sql_cr_tbclient)
         self.con.execute(sql_cr_tbtyp)
         self.con.execute(sql_cr_tbInOut)
+        self.con.execute(sql_cr_tbSysUser)
         try:
             self.con.execute(sql_insert_default_user)
         except Exception, e:
