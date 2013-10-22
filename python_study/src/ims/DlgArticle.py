44fe46feb70d2f8407a5b19737db01cb26e505db
jiang <ericwill@163.com>
2013-10-22 17:37:45 +0800

    [chg]添加了物品表格菜单 [chg]改进了物品类别树的菜单,增加显示所有物品的节点

--- python_study/src/ims/DlgArticle.py
+++ python_study/src/ims/DlgArticle.py
@@ -29,8 +29,6 @@ class DlgArticle(QDialog):
         self.ui.setupUi(self)
         self.setWindowTitle(u'物品信息')
         self.ui.treeWidget.setMaximumWidth(150)
-        self.__updateArticleTree()
-        self.__updateArticleList()
 
         #连接信号和糟
         #用于选择时,双击选中物品,否则弹出物品信息修改窗口
@@ -43,17 +41,23 @@ class DlgArticle(QDialog):
         self.ui.pushButton_add.clicked.connect(self.slotAddArticle)
         self.ui.pushButton_export.clicked.connect(self.slotExport)
         self.ui.pushButton_2.clicked.connect(self.slotDelArticle)
+        self.ui.treeWidget.setSortingEnabled(False)
         self.ui.treeWidget.currentItemChanged.connect(self.slotArticleItemChanged)
         self.ui.treeWidget.itemPressed[QTreeWidgetItem, int].connect(self.slotContextMenu)
         self.ui.treeWidget.setStyleSheet( "QTreeView::item:hover{background-color:rgb(0,255,0,50)} "
                                           "QTreeView:item{border-bottom:1px solid #999999;border-right:1px solid #999999}"
                                           "QTreeView::item:selected{background-color:rgb(255,0,0,100)}");
+
+        self.ui.tableView.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.ui.tableView.customContextMenuRequested.connect(self.slotTableMenu)
         self.ui.tableView.setEditTriggers(QTableWidget.NoEditTriggers)
         self.ui.tableView.setSelectionBehavior(QTableWidget.SelectRows)
         self.ui.tableView.setSelectionMode(QTableWidget.SingleSelection)
         self.ui.tableView.setAlternatingRowColors(True)
+        self.__updateArticleTree()
+        self.__updateArticleList()
 
-        '''如果此窗口用于选择物品'''        
+        '''如果此窗口用于选择物品'''
         if bForChoose:
             '''初始即展开所有项目'''
             self.ui.treeWidget.expandAll()
@@ -64,6 +68,26 @@ class DlgArticle(QDialog):
             self.ui.pushButton_ok.clicked.connect(self.slotChoosed)
         else:
             self.ui.pushButton_ok.hide()
+
+    #物品表格菜单,提供导出,修改,删除功能
+    def slotTableMenu(self):
+        action_export = QAction(u'导出', self)
+        action_del = QAction(u'删除', self)
+        action_modify = QAction(u'修改', self)
+        action_del.triggered.connect(self.slotDelArticle)
+        action_modify.triggered.connect(self.slotModifyArticle)
+        action_export.triggered.connect(self.slotExport)
+        gUser = ims.model.dbSysUser.g_current_user
+        #非管理员用户
+        if None==gUser or not gUser.is_enable_write_all():
+            action_modify.setEnabled(False)
+            action_del.setEnabled(False)
+        menu = QMenu()
+        menu.addAction(action_modify)
+        menu.addAction(action_del)
+        menu.addSeparator()
+        menu.addAction(action_export)
+        menu.exec_(QCursor.pos())
 
     '''选中一项物品'''
     def slotChoosed(self):
@@ -79,34 +103,47 @@ class DlgArticle(QDialog):
 
     '''右键弹出菜单'''
     def slotContextMenu(self, item, col):
-        #if not ims.model.dbSysUser.g_current_user.is_enable_write_all():return
         if qApp.mouseButtons() == Qt.LeftButton: return
-        res = item.data(0, Qt.UserRole+1).toInt()
-        if not res[1]:return
-        depth = res[0]                
         rightMenu = QMenu(u"right")
         action_addType      = QAction(QString(u'新增'), self)
         action_renameType   = QAction(QString(u'改名'), self)
         action_delType      = QAction(QString(u'删除'), self)
         action_addChild     = QAction(QString(u'新增子类'), self)
         action_addArticle   = QAction(QString(u'新增物品'), self)
-        #'''产生菜单,物品节点和类别节点对应菜单不一样'''
-        rightMenu.addAction(action_renameType)
-        rightMenu.addAction(action_delType)
-        rightMenu.addSeparator()
-        rightMenu.addAction(action_addType)
-        #一级节点可以添加子类别
-        if depth ==1:
-            rightMenu.addAction(action_addChild)
-        else:
-            rightMenu.addAction(action_addArticle)
-
-        #'''连接事件'''
+        action_refresh      = QAction(QString(u'刷新'), self)
+         #'''连接事件'''
         action_addType.triggered.connect(self.slotAddType)
         action_renameType.triggered.connect(self.slotRenameType)
         action_delType.triggered.connect(self.slotDelType)
         action_addChild.triggered.connect(self.slotAddChildType)
         action_addArticle.triggered.connect(self.slotAddArticle)
+        action_refresh.triggered.connect(self.__updateArticleTree)
+
+        user = ims.model.dbSysUser.g_current_user
+        if user==None or not user.is_enable_write_all():
+            action_delType.setEnabled(False)
+            action_addType.setEnabled(False)
+            action_addArticle.setEnabled(False)
+            action_renameType.setEnabled(False)
+            action_addChild.setEnabled(False)
+        #获取当前选中节点内容
+        item = self.ui.treeWidget.currentItem()
+        item_data = self.__getItemIdDepth__(item)
+        if not item_data: return
+        #显示所有--节点的弹出菜单
+        if item_data[0]==-1:
+            rightMenu.addAction(action_addType)
+        else:
+            #显示大类型的弹出菜单
+            rightMenu.addAction(action_addType)
+            rightMenu.addAction(action_renameType)
+            rightMenu.addAction(action_delType)
+            if item_data[1]==1:
+                rightMenu.addAction(action_addChild)
+            else:
+                rightMenu.addAction(action_addArticle)
+        rightMenu.addSeparator()
+        rightMenu.addAction(action_refresh)
         #'''显示菜单'''
         rightMenu.exec_(QCursor.pos()) 
     
@@ -189,7 +226,8 @@ class DlgArticle(QDialog):
 
     #'''删除物品分类'''
     def slotDelType(self):
-        if not ims.model.dbSysUser.g_current_user.is_enable_write_all():return
+        user = ims.model.dbSysUser.g_current_user
+        if user==None or not user.is_enable_write_all():return
         item = self.ui.treeWidget.currentItem()
         if item is None: return
         #需先删除所有子类别
@@ -222,7 +260,8 @@ class DlgArticle(QDialog):
 
     #'''对物品类型节点改名'''
     def slotRenameType(self):
-        if not ims.model.dbSysUser.g_current_user.is_enable_write_all():return
+        user = ims.model.dbSysUser.g_current_user
+        if None==user or not user.is_enable_write_all():return
 
         item = self.ui.treeWidget.currentItem()
         if not item : return
@@ -256,14 +295,15 @@ class DlgArticle(QDialog):
             print 'item data is error'
             return
         #'''如果未选中物品节点,则不做任何操作'''
-        if res[1] != 2:
+        if res[1] != 2 and res[0]!=-1:
             #print 'item data-depth !=3'
             return
         self.__updateArticleList()
 
     #添加物品信息
     def slotAddArticle(self):
-        #if not ims.model.dbSysUser.g_current_user.is_enable_write_all():return
+        user = ims.model.dbSysUser.g_current_user
+        if None==user or not user.is_enable_write_all():return
         item = self.ui.treeWidget.currentItem()
         dlg = DlgArticleChange(self)
         if item != None and item.parent() != None:
@@ -283,7 +323,8 @@ class DlgArticle(QDialog):
     #'''修改物料信息到数据库'''
     def slotModifyArticle(self):
         #权限检查
-        #if not ims.model.dbSysUser.g_current_user.is_enable_write_all():return
+        user = ims.model.dbSysUser.g_current_user
+        if None==user or not user.is_enable_write_all():return
         article_id = self.__get_selected_article_id()
         if article_id is None: return
         article = dbArticle().getById(article_id)
@@ -309,14 +350,15 @@ class DlgArticle(QDialog):
         strListHeader.append(u'分类/型号')
         self.ui.treeWidget.setHeaderLabels(strListHeader)
         self.ui.treeWidget.clear()
-        listTypes1 = dbArticleType().getType1() 
+
+        listTypes1 = dbArticleType().getType1()
         #'''插入类别1'''
         for t1 in listTypes1:
             item = QTreeWidgetItem()
             #print t1
             item.setText(0, t1.text)
             item.setData(0, Qt.UserRole,  t1.id)
-            item.setData(0, Qt.UserRole+1, 1)            
+            item.setData(0, Qt.UserRole+1, 1)
             listType2 = dbArticleType().getType2(t1.id)
             self.ui.treeWidget.addTopLevelItem(item)
             #'''插入类别2 '''
@@ -327,6 +369,12 @@ class DlgArticle(QDialog):
                 item2.setData(0, Qt.UserRole+0, t2.id)#id
                 item2.setData(0, Qt.UserRole+1, 2)#深度
                 item.addChild(item2)
+        #插入一个显示所有的项
+        item = QTreeWidgetItem()
+        item.setText(0, u'--[显示所有]--')
+        item.setData(0, Qt.UserRole, -1)
+        item.setData(0, Qt.UserRole+1, 1)
+        self.ui.treeWidget.insertTopLevelItem(0, item)
 
     #更新物品表格中的物品列表
     def __updateArticleList(self):
@@ -337,7 +385,10 @@ class DlgArticle(QDialog):
             if None != res[1]:
                 type_id = res[0]
         if type_id != None:
-            article_list = dbArticle().getArticlesByTypeId(type_id)
+            if type_id == -1:#id 为-1的项用来<显示所有物品列表>
+                article_list = dbArticle().getAll()
+            else:
+                article_list = dbArticle().getArticlesByTypeId(type_id)
         else:
             article_list = []
         labels = [u'ID',u'型号/品名',u'单位',u'封装',u'功能',u'品牌',u'说明']
