72e14607d0cedc6fde358003ad5a5c277258a226
jiang <ericwill@163.com>
2014-02-28 17:16:54 +0800

    [chg]基本完成的功能

--- ImgBlock/src/com/jiang/imgblock/GameView.java
+++ ImgBlock/src/com/jiang/imgblock/GameView.java
@@ -1,8 +1,11 @@
 package com.jiang.imgblock;
 
-import com.example.imgblock.R;
+import java.util.Random;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.*;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,7 +13,15 @@ import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+
+enum GAME_STATUS{
+	STATE_PREVIEW,
+	PLAYING
+}
 
 public class GameView extends View {
 
@@ -18,7 +29,8 @@ public class GameView extends View {
 	public GameView(Context context) {
 		super(context);
 		this.setImage(R.drawable.img0);
-		this.setLevel(3);
+		this.setLevel(3);		
+		this.mStatus = GAME_STATUS.PLAYING;
 	}
 
 	@Override
@@ -26,62 +38,153 @@ public class GameView extends View {
 		
 		Paint paint = new Paint();
 		paint.setColor(Color.BLUE);
-		canvas.drawRect(new Rect(0, 100, 299,300), paint);
+		//canvas.drawRect(new Rect(0, 100, 299,300), paint);
 		//draw bitmap...
-		if(this.mBmp != null){
-			Bitmap bmp = this.mBmp.getBitmap();
-			for(int i=0; i<arrayPos.length; i++)
-			{
-				int x, y;
-				int nSrcPos = arrayPos[i];
-				x = nSrcPos%mLevel;
-				y = nSrcPos/mLevel;
-				Rect src = new Rect(x*bmp.getWidth()/mLevel, y*bmp.getHeight()/mLevel, 
-						(x+1)*bmp.getWidth()/mLevel, (y+1)*bmp.getHeight()/mLevel);
-				
-				int nDstPos = i;
-				x = nDstPos%mLevel;
-				y = nSrcPos/mLevel;
-				Rect dst = new Rect(x*canvas.getWidth()/mLevel, y*canvas.getHeight()/mLevel, 
-						(x+1)*canvas.getWidth()/mLevel, (y+1)*canvas.getHeight()/mLevel);
-				canvas.drawBitmap(this.mBmp.getBitmap(), src, dst, paint);				
-			}
-			paint.setColor(Color.RED);
-			//draw grid
-			for(int i=0; i<this.mLevel; i++)
-				canvas.drawLine(canvas.getWidth()*i/mLevel, 0, canvas.getWidth()*i/mLevel, canvas.getHeight(), paint);
-			for(int i=0; i<this.mLevel; i++)
-				canvas.drawLine(0, canvas.getHeight()*i/mLevel, canvas.getWidth(), canvas.getHeight()*i/mLevel, paint);
-							
+		if(this.mBmp == null) return;
+		Bitmap bmp = this.mBmp;
+		if(mStatus!=GAME_STATUS.PLAYING){
+			Rect src = new Rect(0, 0, bmp.getWidth(), bmp.getHeight());
+			Rect dst = new Rect(0, 0, this.getWidth(), this.getHeight());
+			canvas.drawBitmap(bmp,src, dst, paint);
+			return;			
 		}
+		for(int i=0; i<this.mArrayPos.length; i++)
+		{
+			int x, y;
+			int nSrcPos = this.mArrayPos[i];
+			if(nSrcPos < 0) continue;
+			x = nSrcPos%mLevel;
+			y = nSrcPos/mLevel;
+			Rect src = new Rect(x*bmp.getWidth()/mLevel, y*bmp.getHeight()/mLevel, 
+					(x+1)*bmp.getWidth()/mLevel, (y+1)*bmp.getHeight()/mLevel);
+			
+			int nDstPos = i;
+			x = nDstPos%mLevel;
+			y = nDstPos/mLevel;
+			Rect dst = new Rect(x*canvas.getWidth()/mLevel, y*canvas.getHeight()/mLevel, 
+					(x+1)*canvas.getWidth()/mLevel, (y+1)*canvas.getHeight()/mLevel);
+			canvas.drawBitmap(this.mBmp, src, dst, paint);		
+			paint.setTextSize(40);
+			//canvas.drawText(i+"-"+mArrayPos[i], dst.left+10, dst.top+20, paint);
+		}
+		paint.setColor(Color.RED);
+		//draw grid
+		for(int i=1; i<this.mLevel; i++)
+			canvas.drawLine(canvas.getWidth()*i/mLevel, 0, canvas.getWidth()*i/mLevel, canvas.getHeight(), paint);
+		for(int i=1; i<this.mLevel; i++)
+			canvas.drawLine(0, canvas.getHeight()*i/mLevel, canvas.getWidth(), canvas.getHeight()*i/mLevel, paint);
 		super.onDraw(canvas);
 	}
 	
 	public void setImage(int id){
-		this.mBmp = (BitmapDrawable)getResources().getDrawable(id);
-		this.postInvalidate();
+		this.mBmp = ((BitmapDrawable)getResources().getDrawable(id)).getBitmap();
+		
+		this.invalidate();
 	}
 	//set game level
 	public void setLevel(int nLevel){
 		if(nLevel<3 || nLevel>6) return;
-		this.arrayPos = new int[nLevel*nLevel];
+		this.mArrayPos = new int[nLevel*nLevel];
 		this.mLevel = nLevel;
 		for(int i=0; i<nLevel*nLevel; i++)
-			this.arrayPos[i]=i;
+			this.mArrayPos[i]=i;
+		this.mArrayPos[mLevel*mLevel-1]=-1;
+		this.disorderBlocks();
+	}
+	public void restart(){
+		disorderBlocks();
+		this.mStatus = GAME_STATUS.PLAYING;
+		this.invalidate();
 	}
 	
-	public void touchAt(Point pt){
-		int x = pt.x/(this.getWidth()/mLevel);
-		int y = pt.y/(this.getHeight()/mLevel);
-		
-		for(int i=0; i<arraryPos.length; i++)
-			if(arr)
-		
-	}	
-	//set image from draw
-	private BitmapDrawable mBmp=null;//image for display
-	private int []arrayPos=null;
-	private int mLevel=3;
+	protected void disorderBlocks()
+	{
+		int nBlankPos = getBlankPos();
+		for(int i=0; i<mLevel*50; i++)
+		{
+			Random r = new Random(System.currentTimeMillis());
+			int n = Math.abs(r.nextInt())%4;
+			int nDst = nBlankPos;
+			switch(n)
+			{
+			case 0: if(nBlankPos-1 >= 0) nDst = nBlankPos-1;break;
+			case 1: if(nBlankPos-mLevel >= 0) nDst = nBlankPos-mLevel;break;
+			case 2: if(nBlankPos+1 <mLevel*mLevel) nDst = nBlankPos+1;break;
+			case 3:if(nBlankPos+mLevel < mLevel*mLevel) nDst = nBlankPos+mLevel;break;
+			}
+			if(nDst==nBlankPos) continue;
+			Log.i("info--------", "swith "+nDst+"and "+nBlankPos);
+			mArrayPos[nBlankPos]=mArrayPos[nDst];
+			mArrayPos[nDst]=-1;
+			nBlankPos = nDst;
+		}
+	}
+	//check is game over;
+	public boolean isGameOver(){
+		for(int i=0; i<mArrayPos.length-1; i++){
+			if(mArrayPos[i]!=i)
+				return false;
+		}
+		return true;
+	}
+	//
+	protected int getBlankPos(){
+		int nBlankPos = -1;
+		for(int i=0; i<mArrayPos.length; i++){
+			if(mArrayPos[i]==-1){
+				nBlankPos = i;
+				break;
+			}
+		}
+		return nBlankPos;
+	}
 	
+	public void touchAt(float pixelx, float pixely){
+		int x = (int) (pixelx/(this.getWidth()/mLevel));
+		int y = (int) (pixely/(this.getHeight()/mLevel));
+		int nBlankPos = this.getBlankPos();
+		int nPos = y*mLevel+x;
+		if(nPos==nBlankPos) return;
+		//check is valid pos for switch
+		if(Math.abs(nPos-nBlankPos)!=1 && Math.abs(nBlankPos-nPos)!=mLevel){		
+			return;
+		}else{
+			Log.i("info--------", "swith "+nPos+"and "+nBlankPos);
+			mArrayPos[nBlankPos]=mArrayPos[nPos];
+			mArrayPos[nPos]=-1;			
+			this.invalidate();
+		}
+		if(isGameOver()){			
+			AlertDialog dlg =  new AlertDialog.Builder(this.getContext()).setTitle("确认")
+		 	.setMessage("游戏完成, 是否再来一次？")
+		 	.setPositiveButton("是", new DialogInterface.OnClickListener() {				
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					GameView.this.mStatus = GAME_STATUS.PLAYING;
+					disorderBlocks();
+					GameView.this.invalidate();
+				}
+			})
+		 	.setNegativeButton("否", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					// TODO Auto-generated method stub
+					//System.exit(0);
+					GameView.this.mStatus = GAME_STATUS.STATE_PREVIEW;
+					invalidate();
+				}
+			})
+		 	.show();
+			
+		}
+	}
+	public GAME_STATUS GetStatus() {return mStatus;}
+	public int GetLevel()  { return mLevel;}
+	//set image from draw
+	private Bitmap mBmp=null;//image for display
+	private int []mArrayPos=null;
+	private int mLevel=3;	//游戏等级
+	private GAME_STATUS mStatus;
 	
 }
--- ImgBlock/src/com/jiang/imgblock/MainActivity.java
+++ ImgBlock/src/com/jiang/imgblock/MainActivity.java
@@ -1,18 +1,105 @@
 package com.jiang.imgblock;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.app.Notification.Action;
+import android.content.Intent;
+import android.media.audiofx.AudioEffect.OnControlStatusChangeListener;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnCreateContextMenuListener;
 
 public class MainActivity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.mView = new GameView(this);
 		this.setContentView(this.mView);
 		super.onCreate(savedInstanceState);
 	}
 	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getAction()==MotionEvent.ACTION_DOWN)
+		{
+			this.mView.touchAt(event.getX(),event.getY());
+		}
+		return super.onTouchEvent(event);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		menu.add(1, 0, 0, "Restart");
+		menu.add(1, 1, 0, "Change level");
+		menu.add(1, 2, 0, "Change img");
+		menu.add(1, 3, 0, "Quit");
+		
+		return super.onCreateOptionsMenu(menu);
+	}
+	@Override  
+	 public boolean onKeyDown(int keyCode, KeyEvent event) {  
+	  if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 1) { // 按下的如果是BACK，同时没有重复  
+	   Intent home = new Intent(Intent.ACTION_MAIN);  
+	   home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);  
+	   home.addCategory(Intent.CATEGORY_HOME);  
+	   startActivity(home);  
+	  }  
+	  return super.onKeyDown(keyCode, event);  
+	 }  
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch(item.getItemId())
+		{
+		case 0:{//restart
+			this.mView.restart();
+			this.mView.invalidate();
+			break;
+		}
+		case 1:{//change level
+			for(;;)
+			{
+				int nLevel = Math.abs(new Random().nextInt())%4+3;
+				if(nLevel!=this.mView.GetLevel()) 
+				{
+					this.mView.setLevel(nLevel);
+					this.mView.invalidate();
+					break;
+				}
+			}
+			break;
+		}			
+		case 2:{//change image			
+				int ids[]={
+						R.drawable.img0,
+						R.drawable.img1,
+						R.drawable.img2,
+						R.drawable.img3,
+				};
+				int nid = Math.abs(new Random().nextInt())%4;
+				this.mView.setImage(ids[nid]);
+				this.mView.invalidate();
+				break;
+			}
+		case 3:{//quit
+			System.exit(0);
+			break;
+		}			
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	private GameView mView;
 
 }
